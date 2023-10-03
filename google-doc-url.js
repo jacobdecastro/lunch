87168ed59695069702e457140755e9ec0562274b
Jacob deCastro <jacobcdecastro@gmail.com>
2023-10-03 19:14:04 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1o6lNGsZoafwb3NPEpw6miZD6ZgzqPt8-2A0F7XT1Xv0/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyBofT5pqwNPCWcNAqA_cLb0QxLolsrVsQ4';
